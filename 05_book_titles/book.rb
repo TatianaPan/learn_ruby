2e737cc56a753dd347dff9c5f58908846eeaf7f4
Tatiana Panferova <tatiana.panferova@gmail.com>
2019-12-24 16:45:57 +0100

    Add solutions for 05_book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,22 @@
 class Book
+    attr_reader :title
+    def initialize
+    end
 # write your code here
+    def title=(str)
+        if str.include? ' '
+            arr = str.split(' ')
+            little_words = ['a', 'and', 'or', 'in', 'of', 'the', 'at', 'on', 'over', 'to', 'an']
+            arr.each do |word|
+                if word != arr[0] && little_words.include?(word)
+                    next
+                else
+                    word.capitalize!
+                end
+            end
+            @title = arr.join(' ')
+        else
+            @title = str.capitalize
+        end
+    end
 end
