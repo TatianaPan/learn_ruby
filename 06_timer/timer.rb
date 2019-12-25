3d5db77e502329ac798f3568789a83af1fef9be4
Tatiana Panferova <tatiana.panferova@gmail.com>
2019-12-25 18:26:54 +0100

    Add solutions for 06

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,32 @@
 class Timer
+  attr_reader :seconds
+  attr_writer :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    sec = self.seconds % 60
+    minutes = (self.seconds / 60).to_int
+    if minutes > 59
+      hours = (minutes / 60).to_int
+      minutes = minutes % 60
+    else
+      hours = 0
+    end
+
+    return "#{padded(hours)}:#{padded(minutes)}:#{padded(sec)}"
+  end
+
   #write your code here
+
+  def padded(int)
+    if int < 10
+      return "0#{int}"
+    else
+      return "#{int}"
+    end
+  end
+
 end
